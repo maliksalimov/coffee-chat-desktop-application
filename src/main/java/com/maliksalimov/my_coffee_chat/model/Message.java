746f6db2e8c223d468b82814622d04286a8d068e
maliksalimov <87946333+maliksalimov@users.noreply.github.com>
2026-03-23 18:49:43 +0400

    Added Message model with id, text, sender, and timestamp fields.

---
+++ src/main/java/com/maliksalimov/my_coffee_chat/model/Message.java
@@ -0,0 +1,16 @@
+package com.maliksalimov.my_coffee_chat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Message {
+
+    private Long id;
+    private String text;
+    private String sender;
+    private String timestamp;
+}
