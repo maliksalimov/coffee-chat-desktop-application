cc8fae92dcf96c7844047932ca581da1417f4500
maliksalimov <87946333+maliksalimov@users.noreply.github.com>
2026-03-24 13:49:48 +0400

    Added Chat constructor and sendMessage method to interact with database

--- src/main/java/com/maliksalimov/my_coffee_chat/chat/Chat.java
+++ src/main/java/com/maliksalimov/my_coffee_chat/chat/Chat.java
@@ -1,4 +1,16 @@
 package com.maliksalimov.my_coffee_chat.chat;
 
+import com.maliksalimov.my_coffee_chat.database.DatabaseUtil;
+
 public class Chat {
+    private final CoffeeShop coffeeShop;
+
+    public Chat(CoffeeShop coffeeShop) {
+        this.coffeeShop = coffeeShop;
+    }
+
+    public void sendMessage(String sender, String message){
+        DatabaseUtil.saveMessage(sender, message);
+        coffeeShop.receiveMessage(message);
+    }
 }
