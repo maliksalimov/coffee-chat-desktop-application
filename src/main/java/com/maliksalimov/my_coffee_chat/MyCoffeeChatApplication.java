bde8d977d3c5599969b1c539cb8790aafd1132f4
maliksalimov <87946333+maliksalimov@users.noreply.github.com>
2026-03-24 17:36:14 +0400

    Added main file with database initialization.

---
+++ src/main/java/com/maliksalimov/my_coffee_chat/MyCoffeeChatApplication.java
@@ -0,0 +1,26 @@
+package com.maliksalimov.my_coffee_chat;
+
+import com.maliksalimov.my_coffee_chat.chat.Chat;
+import com.maliksalimov.my_coffee_chat.chat.CoffeeShop;
+import com.maliksalimov.my_coffee_chat.database.DatabaseUtil;
+import com.maliksalimov.my_coffee_chat.ui.ChatApplication;
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import com.maliksalimov.my_coffee_chat.ui.ChatApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoffeeChatApplication {
+
+    public static void main(String[] args) {
+        DatabaseUtil.initializeDatabase();
+
+        CoffeeShop coffeeShop = new CoffeeShop();
+        coffeeShop.startBaristas();
+
+        Chat chat = new Chat(coffeeShop);
+
+        Application.launch(ChatApplication.class, args);
+    }
+
+}
