899ec5fbe0f41a72edaf5dc48c84cf8d8718bcf6
maliksalimov <87946333+maliksalimov@users.noreply.github.com>
2026-03-24 17:36:22 +0400

    test file added

---
+++ src/test/java/com/maliksalimov/my_coffee_chat/MyCoffeeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maliksalimov.my_coffee_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoffeeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
